d227ae3a82fb371cb5c68d88006662a85a4afa56
LfrPaul <paulo.azerty59@gmail.com>
2024-09-16 17:33:12 +0200

    init commit

---
+++ test/unit_test.py
@@ -0,0 +1,16 @@
+import unittest
+from donnedessouslampe.donne import donne_des_sous
+
+
+class MyTestCase(unittest.TestCase):
+    def test_something(self):
+        compte_en_banque = donne_des_sous(8, 10)
+        self.assertEqual(18, compte_en_banque)  # add assertion here
+
+    def test_something1(self):
+        compte_en_banque = donne_des_sous(12, 10)
+        self.assertEqual(24, compte_en_banque)  # add assertion here
+
+
+if __name__ == '__main__':
+    unittest.main()
